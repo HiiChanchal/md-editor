a3486fceb0a70be682b7a7208aaaa57ccd7ff2f6
Chanchal Kumar Chandan <chanchal@pickright.in>
2024-09-20 13:43:34 +0530

    Fixed cors error

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3001
+const port = process.env.port || 5001
 const mdeditorPath = require('./routes/md.html.route')
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.use('/text/path',mdeditorPath)
 
