2108c46ad14f970d9de9d0ba3b6d2814f0c93182
Chanchal Kumar Chandan <chanchal@pickright.in>
2024-09-20 11:12:03 +0530

    request to json

--- controllers/md.html.controller.js
+++ controllers/md.html.controller.js
@@ -1,6 +1,6 @@
 const mdService = require("../services/md.html.service")
 exports.textToConvert=(req,res)=>{
-    const userText = req.body;
+    const {userText} = req.body;
     if(!userText){
         return res.status(400).json({error : "Invalid text, Please enter text and Try again"});
     }
