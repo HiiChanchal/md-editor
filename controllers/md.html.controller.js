6ce3af311a5a70e690623661ad30859a6dbab67d
Chanchal Kumar Chandan <chanchal@pickright.in>
2024-09-20 13:44:57 +0530

    Fixed issue in body data parsing

--- controllers/md.html.controller.js
+++ controllers/md.html.controller.js
@@ -1,9 +1,11 @@
 const mdService = require("../services/md.html.service")
-exports.textToConvert=(req,res)=>{
-    const {userText} = req.body;
-    if(!userText){
+exports.textToConvert = (req,res) => {
+    const { markdown } = req.body;
+
+    if(!markdown){
         return res.status(400).json({error : "Invalid text, Please enter text and Try again"});
     }
-    const textData = mdService.htmlTextConversion(userText);
-    res.json({textData});
+
+    const textData = mdService.htmlTextConversion(markdown);
+    return res.json({ textData });
 }
--- routes/md.html.route.js
+++ routes/md.html.route.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mdhtmlController = require('../controllers/md.html.controller')
 
-router
-.post(
-    '/mdeditor',
-    mdhtmlController.textToConvert
-)
+router.post('/mdeditor', mdhtmlController.textToConvert);
 
 module.exports = router;
